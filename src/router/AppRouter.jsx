c43322f44db35c458c2fa78049a1eae823a307fe
Stefano <stefanop21@outlook.es>
2024-02-01 00:55:36 -0500

    create(src/router): Componente PrivateRoute.jsx

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import { TarotRoutes } from '../tarot/routes/TarotRoutes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path="login" element={<LoginPage />} />
 
-      <Route path="/*" element={<TarotRoutes />} />
+      <Route
+        path="/*"
+        element={
+          <PrivateRoute>
+            <TarotRoutes />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 };
