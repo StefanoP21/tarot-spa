70c2de02683c72241f1e4f7a0fa3344deb280cf5
Stefano <stefanop21@outlook.es>
2024-02-04 00:01:32 -0500

    update(src/tarot/components): Componente TarotList.jsx

--- src/tarot/components/TarotList.jsx
+++ src/tarot/components/TarotList.jsx
@@ -8,7 +8,7 @@ export const TarotList = ({ type }) => {
   return (
     <div className="bg-gray-700">
       <div className="mx-auto max-w-xl px-4 py-10 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
-        <div className="my-4 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="mt-4 mb-20 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {tarot.map((card) => (
             <TarotCard key={card.id} {...card} />
           ))}
