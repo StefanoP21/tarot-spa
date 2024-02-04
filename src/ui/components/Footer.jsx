690009085efa9b305e3156c749c24c8454b5dcc8
Stefano <stefanop21@outlook.es>
2024-02-04 00:05:18 -0500

    update(src/ui/components): Componente Footer.jsx

--- src/ui/components/Footer.jsx
+++ src/ui/components/Footer.jsx
@@ -11,7 +11,7 @@ export const Footer = () => {
     <footer className="fixed bottom-0 left-0 z-20 w-full bg-gray-800 py-2">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{' '}
+          © 2024{' '}
           <a
             href="https://stefano.vercel.app/"
             className="hover:underline"
