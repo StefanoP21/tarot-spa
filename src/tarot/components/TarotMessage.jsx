dbee529e1f6ee9bf5d1eee7f43b5518df43837e2
Stefano <stefanop21@outlook.es>
2024-02-21 16:06:39 -0500

    update(src/tarot/components): Componente TarotMessage.jsx

--- src/tarot/components/TarotMessage.jsx
+++ src/tarot/components/TarotMessage.jsx
@@ -2,7 +2,7 @@ export const TarotMessage = ({ title, message, color }) => {
   return (
     <div className="mt-10">
       <div
-        className={`${color} "px-4 py-5 sm:px-6 sm:py-6 sm:rounded-lg sm:shadow-xl"`}
+        className={`${color} px-4 py-5 sm:px-6 sm:py-6 sm:rounded-lg sm:shadow-xl`}
       >
         <h3 className="text-xl font-semibold leading-6 text-white">{title}</h3>
         <div className="mt-2 text-md text-gray-100">
